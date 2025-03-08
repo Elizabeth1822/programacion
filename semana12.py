e7a111ba6ae9246fc6b901a5a5524f010da1d294
Elizabeth1822 <me.gonzalezb@uea.edu.ec>
2025-03-08 17:58:46 -0600

    Add files via upload
    Iteración de arreglos multidimensionales con bucles anidados

---
+++ semana12.py
@@ -0,0 +1,15 @@
+# Crear una matriz 3D con las temperaturas diarias de Guayaquil, Quito y Cuenca durante varias semanas
+# Ejemplo: 3 ciudades (Guayaquil, Quito, Cuenca), 7 días de la semana, 4 semanas
+
+# Matriz 3D: [ciudades][días de la semana][semanas]
+temperaturas = [
+    [  # Guayaquil (Ciudad 1)
+        [30, 31, 32, 31, 30, 29, 30],  # Semana 1
+        [31, 32, 33, 32, 31, 30, 31],  # Semana 2
+        [30, 31, 32, 31, 30, 29, 30],  # Semana 3
+        [31, 32, 33, 32, 31, 30, 31],  # Semana 4
+    ],
+    [  # Quito (Ciudad 2)
+        [15, 16, 17, 16, 15, 14, 15],  # Semana 1
+        [16, 17, 18, 17, 16, 15, 16],  # Semana 2
+        [15, 16, 17, 16, 15, 14, 15],  # Semana 3
